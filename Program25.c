6132c11b550a716611f849c527224ff13ff4a6fd
Gaurav Katwate <gauravkatwate5@gmail.com>
2023-07-20 16:03:28 +0530

    C_Program

--- Program25.c
+++ Program25.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
+float Percentage(float iNo1, float iNo2)
+{
+    float fAns = 0.0;
+
+    fAns = (iNo2 * 100) / iNo1;
+
+    return fAns;
+}
+
 int main()
 {
+    float iTotal = 0, iObtained = 0;
+    float fret = 0.0;
+
+    printf("Enter Total Marks : \n");
+    scanf("%d", &iTotal);
+
+    printf("Enter Obtained Marks : \n");
+    scanf("%d", &iObtained);
+
+    fret = Percentage(iTotal, iObtained);
+
+    printf("Persentage of student is : %f%%", fret);
+
     return 0;
 }
